3d75166510f1e4108b4b6ce4fe4a9379e3ba2874
zane <97927259+Supraboy981322@users.noreply.github.com>
2025-11-25 18:37:59 -0600

    more helper funcs

--- main.go
+++ main.go
@@ -29,6 +29,22 @@ func ParseIgn(input string) Map {
 	return res
 }
 
+func GetValue(key any, gomn Map) (interface{}, bool) {
+	if gomn[key] == nil {
+		return nil, false
+	}
+	return gomn[key], true
+}
+
+func GetValueFromStr(key any, gomnStr string) (interface{}, error) {
+	gomnMap, err := Parse(gomnStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return gomnMap[key], nil 
+}
+
 func ParseFile(file string) (Map, error) {
 	fileBytes, err := os.ReadFile(file)
 	if err != nil {
